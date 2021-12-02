1035f66a538a30b097b7a265e6006131a2cbe258
Boris Ahumada <boris.ahumada@laposte.net>
2021-12-02 15:33:42 +0100

    Exo6

--- src/test/java/java8/ex06/Function_06_Test.java
+++ src/test/java/java8/ex06/Function_06_Test.java
@@ -18,7 +18,7 @@ public class Function_06_Test {
     // TODO la méthode retourne une chaîne de caractères de la forme [age=<AGE>] (exemple : [age=12])
     String formatAge(Supplier<Person> supplier) {
         // TODO
-        return null;
+        return "[age=" + supplier.get().getAge()+"]";
     }
     // end::formatAge[]
 
@@ -26,7 +26,8 @@ public class Function_06_Test {
     @Test
     public void test_supplier_formatAge() throws Exception {
         // TODO compléter le test unitaire pour qu'il soit passant
-        String result = formatAge(null);
+    	Supplier<Person> supp = () -> new Person("", "", 35, "");
+        String result = formatAge(supp);
 
         assert result.equals("[age=35]");
     }
